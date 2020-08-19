720647119e3ee3218ff9e0de37e68df2656b3bee
Yeonwoo Lim <1213terry@naver.com>
2020-08-19 16:01:37 +0900

    Apply .gitignore

--- _site/assets/js/lunr/lunr-store.js
+++
@@ -1,7 +0,0 @@
-var store = [{
-        "title": "테스트입니다.",
-        "excerpt":"print(\"Hello, world!\")  ","categories": [],
-        "tags": [],
-        "url": "http://localhost:4000/blog/",
-        "teaser": null
-      }]
