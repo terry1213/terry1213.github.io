912eeedc16ef6f43c464576281c1b93b7a135e24
Yeonwoo Lim <1213terry@naver.com>
2020-08-19 15:14:06 +0900

    설정 세팅 및 테스트 포스트 업로드
    설정 세팅 및 테스트 포스트 업로드

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -1,1 +1,7 @@
-var store = []
+var store = [{
+        "title": "테스트입니다.",
+        "excerpt":"print(\"Hello, world!\")  ","categories": [],
+        "tags": [],
+        "url": "http://localhost:4000/blog/",
+        "teaser": null
+      }]
